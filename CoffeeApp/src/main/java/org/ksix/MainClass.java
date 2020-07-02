afb56deec2a4ac6baedbf177cf31405492b397fe
MarkTiukov <tyukov.mark@gmail.com>
2020-07-03 00:16:56 +0300

    Empty window

--- CoffeeApp/src/main/java/org/ksix/MainClass.java
+++ CoffeeApp/src/main/java/org/ksix/MainClass.java
@@ -1,9 +1,14 @@
 package org.ksix;
 
+import org.ksix.mygraphics.MyInterface;
+
 public class MainClass {
 
     public static void main(String[] args) {
         Uploader.uploadEverything();
+        MyInterface my_interface = new MyInterface();
+        my_interface.showWindow();
+
     }
 
 }
--- CoffeeApp/src/main/java/org/ksix/mygraphics/MyInterface.java
+++ CoffeeApp/src/main/java/org/ksix/mygraphics/MyInterface.java
@@ -3,12 +3,13 @@ package org.ksix.mygraphics;
 import javax.swing.*;
 
 public class MyInterface implements Interface {
-    private static final int windowSize = 1000;
     private final JFrame window = new JFrame("War Game");
 
     public MyInterface() {
+        // TODO test full-screen mode on Windows
         this.window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.window.setSize(windowSize, windowSize);
+        this.window.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        // this.window.setUndecorated(true);
         this.window.setLayout(null);
     }
 
